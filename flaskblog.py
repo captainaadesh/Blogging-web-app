31e2e71a3741c647055c9de7342aa75a7945ef08
Aadesh Mirajkar <mirajkaraadesh@gmail.com>
2020-05-19 21:55:25 +0530

    added database

--- flaskblog.py
+++ flaskblog.py
@@ -1,9 +1,37 @@
 from flask import Flask, render_template, url_for, redirect, flash
 from forms import RegistrationForm, LoginForm
-app = Flask(__name__)
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
+app = Flask(__name__)
 #secret key for protection againt cookies
 app.config['SECRET_KEY'] = 'e194fb5513a73312'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+db = SQLAlchemy(app) #instance of database
+
+
+class User(db.Model):  #user class to hold the users 
+	id = db.Column(db.Integer, primary_key=True)
+	username = db.Column(db.String(10), unique=True, nullable=False)
+	email = db.Column(db.String(100), unique=True, nullable=False)
+	image_file = db.Column(db.String(20), nullable=False, default='default.jpg')
+	password = db.Column(db.String(20), nullable=False)
+
+	def __repr__(self): #this method tells how users object is printed out
+		return f"User('{self.username}', '{self.email}', '{self.image_file}')"
+
+
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    date_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    content = db.Column(db.Text, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+
+    def __repr__(self):
+        return f"Post('{self.title}', '{self.date_posted}')"
+
+
 
 post = [
 	{
