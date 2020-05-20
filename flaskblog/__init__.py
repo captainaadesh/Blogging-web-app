baa69c22693dda3056d94b7d9144fff396037714
Aadesh Mirajkar <mirajkaraadesh@gmail.com>
2020-05-20 21:21:55 +0530

    fixed user registration

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -1,10 +1,14 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt 
+
 
 app = Flask(__name__)
 #secret key for protection againt cookies
 app.config['SECRET_KEY'] = 'e194fb5513a73312'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 db = SQLAlchemy(app) #instance of database
+bcrypt = Bcrypt(app) 
+
 
 from flaskblog import routes
