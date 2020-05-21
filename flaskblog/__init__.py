0844e026d98d0730a2db4d7f08c8a6c772345dbc
Aadesh Mirajkar <mirajkaraadesh@gmail.com>
2020-05-21 14:30:14 +0530

    fixed bugs

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -1,14 +1,14 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt 
-
+from flask_login import LoginManager
 
 app = Flask(__name__)
 #secret key for protection againt cookies
 app.config['SECRET_KEY'] = 'e194fb5513a73312'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 db = SQLAlchemy(app) #instance of database
-bcrypt = Bcrypt(app) 
-
+bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
 
 from flaskblog import routes
