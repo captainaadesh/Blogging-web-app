830e227a3d03b9607980b861c0848c96a0351fc6
Aadesh Mirajkar <mirajkaraadesh@gmail.com>
2020-05-21 18:54:22 +0530

    registration error fixed

--- flaskblog/routes.py
+++ flaskblog/routes.py
@@ -26,12 +26,12 @@ post = [
 	}
 ]
 
-@app.route('/')
-@app.route('/home')
+@app.route("/")
+@app.route("/home")
 def home():
     return render_template('home.html', posts=post)
 
-@app.route('/about')
+@app.route("/about")
 def about():
 	return render_template('about.html', title = 'About')
 
@@ -40,15 +40,15 @@ def about():
 def register():
 	if current_user.is_authenticated:
 		return redirect(url_for('home'))
-		form = RegistrationForm()
-		if form.validate_on_submit():
-			hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
+	form = RegistrationForm()
+	if form.validate_on_submit():
+		hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
 		user = User(username=form.username.data, email=form.email.data, password=hashed_password)
 		db.session.add(user)
 		db.session.commit()
 		flash('Account created', 'success')
 		return redirect(url_for('login'))
-		return render_template('register.html', title='Register', form=form)
+	return render_template('register.html', title='Register', form=form)
 
 
 @app.route("/login", methods=['GET', 'POST'])
